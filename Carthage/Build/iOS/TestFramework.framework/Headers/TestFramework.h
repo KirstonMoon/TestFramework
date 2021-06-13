ddbfa15694c9514dbdfb21983c18cac046163390
Kirill Magerya <fresno.super@gmail.com>
2021-06-13 22:38:55 +0700

    push

---
+++ Carthage/Build/iOS/TestFramework.framework/Headers/TestFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestFramework.h
+//  TestFramework
+//
+//  Created by Kirill Magerya on 13.06.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFramework.
+FOUNDATION_EXPORT double TestFrameworkVersionNumber;
+
+//! Project version string for TestFramework.
+FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
+
+
