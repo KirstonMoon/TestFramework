e67e5b30a9e6487fa57bfd69a2c519085b30f294
Kirill Magerya <fresno.super@gmail.com>
2021-06-13 22:24:59 +0700

    push

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Kirill Magerya on 13.06.2021.
+//
+
+import UIKit
+import TestFramework
+
+class ViewController: UIViewController {
+
+    let test = TestFramework()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TestFramework/TestFramework.swift
@@ -0,0 +1,15 @@
+//
+//  TestFramework.swift
+//  TestFramework
+//
+//  Created by Kirill Magerya on 13.06.2021.
+//
+
+import Foundation
+
+open class TestFramework {
+    
+    public init() {
+        
+    }
+}
